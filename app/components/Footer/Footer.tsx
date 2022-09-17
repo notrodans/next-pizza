7733ff4f4e50921088e570252e83b884c0e8d0c5
notrodans <notrodans@gmail.com>
2022-09-17 15:01:06 +0300

    update

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,13 @@
+import Pagination from '@components/Pagination/Pagination'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 
 const Footer: FC = () => {
+  const { pathname } = useRouter()
+
   return (
     <footer>
-      <span>Футер</span>
+      <div className='footer__container'>{pathname.includes('cart') ? '' : <Pagination />}</div>
     </footer>
   )
 }
---
+++ app/components/Pagination/Pagination.tsx
@@ -0,0 +1,27 @@
+import { selectFilter, setPage } from 'app/redux/slices/filter/filter.slice'
+import React, { FC, memo } from 'react'
+import ReactPaginate from 'react-paginate'
+
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+
+import styles from './Pagination.module.scss'
+
+const Pagination: FC = () => {
+  const dispatch = useAppDispatch()
+  const { currentPage } = useAppSelector(selectFilter)
+
+  return (
+    <ReactPaginate
+      className={styles.pagination}
+      breakLabel='...'
+      nextLabel='>'
+      previousLabel='<'
+      onPageChange={e => dispatch(setPage(e.selected + 1))}
+      pageRangeDisplayed={4}
+      pageCount={3}
+      forcePage={currentPage - 1}
+    />
+  )
+}
+
+export default memo(Pagination)
